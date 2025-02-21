24729f2ce87c4c0376b0e647bb357a5aca4d81fc
Amir Razvi <amirrazvi8@gmail.com>
2025-02-21 23:24:16 +0530

    Refactor OTP generation logic to improve error handling and response structure

--- src/routes/email.route.js
+++ src/routes/email.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { sendContactEmail,sendOtp } from "../controllers/email.controller.js";
+import { sendContactEmail } from "../controllers/email.controller.js";
 
 const router = Router();
 
