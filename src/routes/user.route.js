0f2605a148134fcb4cfe454a7d9f99696198c407
Amir Razvi <amirrazvi8@gmail.com>
2025-02-22 00:06:03 +0530

    Refactor user routes to streamline middleware usage and improve route definitions

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,29 +1,23 @@
 import Router from "express";
-import express from "express";
-import cors from "cors";
-import { registerUser, loginUser, logoutUser,verifyOtp, resetPassword } from "../controllers/user.controller.js";
+
+import { registerUser, loginUser, logoutUser, verifyOtp, resetPassword } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-router.use(
+const router = Router();
+
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/verify-otp").post(verifyOtp);
+router.route("/reset-password").post(resetPassword);
+
+router.post(
+    "/generate-otp",
     cors({
         origin: "https://luxora-shop.vercel.app",
         credentials: true,
     }),
     generateOtp
 );
-
-const router = Router();
-
-router.route('/register').post(registerUser);
-router.route('/login').post(loginUser);
-router.route('/logout').post(verifyJWT,logoutUser);
-router.route('/verify-otp').post(verifyOtp);
-router.route('/reset-password').post(resetPassword);
-router.route('/generate-otp').post(generateOtp);
-
-
-
-
-
 
 export default router;
